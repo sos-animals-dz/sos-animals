b2e02ae85ded75a35c6de86417a2209852e9f688
AM-77 <amine.griche77@protonmail.ch>
2020-07-08 23:34:35 +0200

    integrate the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMapGl, { PointerEvent, Marker } from 'react-map-gl'
+
+import pin from './pin.svg'
 
 function App() {
+
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    zoom: 12,
+    longitude: 3.04197,
+    latitude: 36.7525
+  })
+
+  const [markers, setMarkers] = useState([
+    [3.04197, 36.7525]
+  ])
+
+  const selected = (e:PointerEvent) => {
+    setMarkers([...markers, e.lngLat])
+  }
+
+  console.log(process.env.REACT_APP_MAPBOX_TOKEN)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ReactMapGl 
+        {...viewport}
+        mapboxApiAccessToken={'pk.eyJ1IjoiYW0tNzciLCJhIjoiY2tjYTNtYTlzMXJtOTJ2bG1jcmJ0M3l0MSJ9.0cR_Hn8TgDIxd_etq3E-OQ'}
+        mapStyle={'mapbox://styles/am-77/ckca59r03371f1ileiqk2x44x'}
+        onViewportChange={(viewport: any) => setViewport(viewport)}
+        onDblClick={(e) => selected(e)}
         >
-          Learn React
-        </a>
-      </header>
+        {
+          markers.map((marker: any, index:number) => (
+            <Marker 
+              key={index}
+              longitude={marker[0]}
+              latitude={marker[1]}
+              >
+              <div className="pin">
+                <img src={pin} alt='pin' />
+              </div>
+            </Marker>
+          ))
+        }
+      </ReactMapGl>
     </div>
   );
 }
