3f4587a142c280dd0cf7409b6b6d4a01c0e6e996
AM-77 <amine.griche77@protonmail.ch>
2020-07-12 16:40:42 +0200

    update Search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import Search from './Search'
 import logo from '../assets/logo.jpg'
+import { ViewportProps } from 'react-map-gl'
 
-export default class Navbar extends Component {
+interface IProps {
+  setViewport: (viewport: ViewportProps) => void
+}
+
+export default class Navbar extends Component<IProps> {
   render() {
+    const { setViewport } = this.props
     return (
       <div className="navbar-container">
         <div className="logo">
           <img src={logo} alt='SOS Animal' />
         </div>
-        <Search />
+        <Search setViewport={setViewport} />
       </div>
     )
   }
