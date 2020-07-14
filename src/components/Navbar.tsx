968eb5a28b0c256c72c2e15cda15a873790a8443
AM-77 <amine.griche77@protonmail.ch>
2020-07-14 03:00:51 +0200

    add add-animal button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
-import Search from './Search'
-import logo from '../assets/logo.jpg'
 import { ViewportProps } from 'react-map-gl'
+import Search from './Search'
+
+import logo from '../assets/logo.jpg'
+import add from '../assets/add.jpg'
 
 interface IProps {
   setViewport: (viewport: ViewportProps) => void
+  addAnimal: () => void
+  isAddAnimal: boolean
 }
 
 export default class Navbar extends Component<IProps> {
   render() {
-    const { setViewport } = this.props
+    const { setViewport, addAnimal, isAddAnimal } = this.props
     return (
       <div className="navbar-container">
         <div className="logo">
           <img src={logo} alt='SOS Animal' />
         </div>
+        <button className={`add-animal ${isAddAnimal ? 'active' : ''}`} onClick={addAnimal}>
+          <img src={add} alt='add animal' />
+        </button>
         <Search setViewport={setViewport} />
       </div>
     )
