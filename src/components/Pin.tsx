5ea4c280dfcbd7d30702128ee914b3c72e05e4ac
AM-77 <amine.griche77@protonmail.ch>
2020-07-14 18:10:27 +0200

    replace index with id

--- src/components/Pin.tsx
+++ src/components/Pin.tsx
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import pin from '../assets/pin.svg'
 
 interface IProps {
-  index: number
+  id: number
 }
 
 export default class Pin extends Component<IProps> {
 
-  onPinClick = (index: number) => {
-    console.log(index)
+  onPinClick = (id: number) => {
+    console.log(id)
+    // display infos
   }
 
   render() {
-    const { index } = this.props
+    const { id } = this.props
     return (
       <div className="pin-container">
-        <div onClick={() => this.onPinClick(index)} className="pin" style={{ position: 'relative', cursor: 'pointer'}}>
+        <div onClick={() => this.onPinClick(id)} className="pin" style={{ position: 'relative', cursor: 'pointer'}}>
           <img src={pin} alt='pin' style={{ position: 'absolute', top: '-50%' }} />
         </div>
       </div>
