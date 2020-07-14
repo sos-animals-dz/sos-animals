72517f165593103d205755fb0c07fed9694e059f
AM-77 <amine.griche77@protonmail.ch>
2020-07-14 15:00:35 +0200

    add closeSidebar, isSideOpen & toggleSide

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 
 import AddAnimal from './AddAnimal'
 
-export default class Sidebar extends Component {
+interface IProps {
+  isSideOpen: boolean
+  toggleSide: (isSideOpen: boolean) => void
+}
+
+export default class Sidebar extends Component<IProps> {
+  constructor(props: IProps) {
+    super(props)
+  }
+
   render() {
+    const { isSideOpen, toggleSide } = this.props
+
     return (
-      <div className="sidebar-container">
-        <AddAnimal />
+      <div className={`sidebar-container ${isSideOpen ? 'side-open': ''}`}>
+        <AddAnimal toggleSide={toggleSide} />
         {/* Display animal  */}
       </div>
     )
