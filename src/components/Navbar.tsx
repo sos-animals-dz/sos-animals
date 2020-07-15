2d93e21e26caa1db4b1d1c9ec6ef28b4ddadfd92
AM-77 <amine.griche77@protonmail.ch>
2020-07-15 20:09:06 +0200

    fix enable/disable animal-mode button click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { ViewportProps } from 'react-map-gl'
 import Search from './Search'
+import IAnimal from '../interfaces/IAnimal'
 
 import logo from '../assets/logo.jpg'
 import add from '../assets/add.jpg'
@@ -10,17 +11,29 @@ interface IProps {
   setViewport: (viewport: ViewportProps) => void
   toggleIsAddAnimal: () => void
   isAddAnimal: boolean
+  isSideOpen: false | IAnimal | 'add-animal'
 }
 
 export default class Navbar extends Component<IProps> {
+
+  onAddAnimalClick = () => {
+    const { isSideOpen, toggleIsAddAnimal } = this.props
+    if (isSideOpen !== "add-animal") {
+      toggleIsAddAnimal()
+    }
+  }
+
   render() {
-    const { setViewport, toggleIsAddAnimal, isAddAnimal } = this.props
+    const { setViewport, isAddAnimal } = this.props
     return (
       <div className="navbar-container">
         <div className="logo">
           <img src={logo} alt='SOS Animal' />
         </div>
-        <button className={`add-animal ${isAddAnimal ? 'active' : ''}`} onClick={toggleIsAddAnimal}>
+        <button 
+          title={`${isAddAnimal ? 'disable add-animal mode' : 'enable add-animal mode'}`} 
+          className={`add-animal ${isAddAnimal ? 'active' : ''}`} 
+          onClick={this.onAddAnimalClick}>
         {
           isAddAnimal ?
             <img src={addActive} alt='add animal' /> 
