327d7529d424f5a76439a0766e23f8e81f2bf9cc
AM-77 <amine.griche77@protonmail.ch>
2020-07-19 16:03:51 +0200

    add report form

--- src/components/DisplayAnimal.tsx
+++ src/components/DisplayAnimal.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, ChangeEvent } from 'react'
 import IAnimal from '../interfaces/IAnimal'
 
 import noImage from '../assets/no-img-available.jpg'
@@ -9,9 +9,43 @@ interface IProps {
   reportAnimal: (id: number) => void
 }
 
-export default class DisplayAnimal extends Component<IProps> {
+interface IState {
+  report: string
+  isReporting: boolean
+}
+
+export default class DisplayAnimal extends Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+      report: "",
+      isReporting: false
+    }
+  }
+
+  onReportTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const report = e.currentTarget.value
+    this.setState({ report })
+  }
+
+  toggleReport = (isReporting: boolean) => this.setState({ isReporting })
+
+  closeSlide = () => {
+    const { toggleSide } = this.props
+    this.setState({ report: "", isReporting: false }, () => toggleSide(false))
+  }
+
+  submitReport = () => {
+    const { animal: { id }, reportAnimal } = this.props
+    const { report } = this.state
+
+    console.log(id, report)
+  }
+
   render() {
-    const { animal: { type, picture, description, id }, toggleSide, reportAnimal } = this.props
+    const { animal: { type, picture, description } } = this.props
+    const { report, isReporting } = this.state
     return (
       <div className="display-animal-container">
         <div className="animal-type">
@@ -30,8 +64,22 @@ export default class DisplayAnimal extends Component<IProps> {
           <p>{description}</p>
         </div>
         <div className="footer">
-          <button className="close" onClick={() => toggleSide(false)}>close</button>
-          <button className="report" onClick={() => reportAnimal(id)}>report</button>
+          <button className="close" onClick={this.closeSlide}>close</button>
+          <button className="report" onClick={() => this.toggleReport(true)}>report</button>
+        </div>
+        <div className={`report-container ${isReporting ? 'active' : ''}`}>
+          <h4 className="title">Your report: </h4>
+          <textarea 
+            value={report}
+            name="report"
+            placeholder='Please justify why are you reporting this.(at least 5 words)'
+            onChange={this.onReportTextareaChange}
+            ></textarea>
+
+            <div className="report-buttons">
+              <button className="submit" onClick={this.submitReport}>submit</button>
+              <button className="cancel" onClick={() => this.toggleReport(false)}>cancel</button>
+            </div>
         </div>
       </div>
     )
