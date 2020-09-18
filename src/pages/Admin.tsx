d9bf76f2db6d359fbfb5e3d1506f70d639d3968a
AM-77 <amine.griche77@protonmail.ch>
2020-09-18 23:31:06 +0100

    add the animals list

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+
+import ListAnimals from '../components/ListAnimals'
 import Navbar from '../components/Navbar'
 
-export default class Admin extends Component {
+import IAnimal from '../interfaces/IAnimal'
+import { User as IUser } from "firebase"
+
+interface IProps {
+  animals: IAnimal[]
+  loggedUser: IUser
+  isLoadingAnimals: boolean
+  setToast: (isSuccess: boolean, message: string, isHidden: boolean) => void
+  loadAnimals: ( callback?:() => void ) => void
+}
+
+export default class Admin extends Component<IProps> {
   render() {
+    const { animals, isLoadingAnimals, loggedUser, setToast, loadAnimals } = this.props
     return (
-      <div>
-        <Navbar isAdmin={true} />
+      <div className="admin-container">
+        <Navbar isLoadingAnimals={isLoadingAnimals} loggedUser={loggedUser} isAdmin />
+        <div className="dashboard">
+          <ListAnimals 
+            setToast={setToast} 
+            animals={animals} 
+            loadAnimals={loadAnimals}
+            />
+        </div>
       </div>
     )
   }
