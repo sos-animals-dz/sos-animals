29e4108b4479c8be42ceb1d2d18e55c270347d9b
AM-77 <amine.griche77@protonmail.ch>
2020-11-14 12:51:45 +0100

    update Map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ReactMapGl, {
   PointerEvent,
   ViewportProps,
@@ -18,80 +18,78 @@ interface IProps {
   displayAnimal: (id: number) => void;
 }
 
-setRTLTextPlugin(
-  'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
-  (err) => {
-    if (err) console.log('[!] Error@Map.setRTLTextPlugin', err);
-  },
-  true
-);
+export default function Map(props: IProps) {
+  useEffect(() => {
+    try {
+      setRTLTextPlugin(
+        'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+        (err) => {
+          if (err) console.log('[!] Error@Map.setRTLTextPlugin', err);
+        },
+        true
+      );
+    } catch (error) {
+      console.log('Error@Map: ', error);
+    }
+  }, []);
 
-export default class Map extends Component<IProps> {
-  addMarker = (e: PointerEvent) => {
-    const { addAnimalMarker } = this.props;
+  const addMarker = (e: PointerEvent) => {
+    const { addAnimalMarker } = props;
     addAnimalMarker({ longitude: e.lngLat[0], latitude: e.lngLat[1] });
   };
 
-  render() {
-    const {
-      viewport,
-      animals,
-      setViewport,
-      isSideOpen,
-      displayAnimal,
-    } = this.props;
+  const { viewport, animals, setViewport, isSideOpen, displayAnimal } = props;
 
-    const {
-      latitude,
-      longitude,
-      zoom,
-      bearing,
-      pitch,
-      altitude,
-      maxZoom,
-      maxPitch,
-      minPitch,
-      transitionDuration,
-      transitionInterpolator,
-      transitionInterruption,
-    } = viewport;
+  const {
+    latitude,
+    longitude,
+    zoom,
+    bearing,
+    pitch,
+    altitude,
+    maxZoom,
+    maxPitch,
+    minPitch,
+    transitionDuration,
+    transitionInterpolator,
+    transitionInterruption,
+  } = viewport;
 
-    return (
-      <div className={`map-gl-container ${isSideOpen ? 'side-open' : ''}`}>
-        <ReactMapGl
-          latitude={latitude}
-          longitude={longitude}
-          zoom={zoom}
-          bearing={bearing}
-          pitch={pitch}
-          altitude={altitude}
-          maxZoom={maxZoom}
-          maxPitch={maxPitch}
-          minPitch={minPitch}
-          transitionDuration={transitionDuration}
-          transitionInterpolator={transitionInterpolator}
-          transitionInterruption={transitionInterruption}
-          width="100vw"
-          height="100vh"
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          mapStyle="mapbox://styles/amine77/ckgkbj8w51q0x19jxhs9q0n53"
-          onViewportChange={(vport: ViewportProps) => setViewport(vport)}
-          onDblClick={(e) => this.addMarker(e)}
-          minZoom={12}
-          mapOptions={{
-            maxBounds: [
-              [2.65, 36.45],
-              [3.8, 36.95],
-            ],
-          }}
-        >
-          <Markers
-            isSideOpen={isSideOpen}
-            animals={animals}
-            displayAnimal={displayAnimal}
-          />
-        </ReactMapGl>
-      </div>
-    );
-  }
+  return (
+    <div className={`map-gl-container ${isSideOpen ? 'side-open' : ''}`}>
+      <ReactMapGl
+        latitude={latitude}
+        longitude={longitude}
+        zoom={zoom}
+        bearing={bearing}
+        pitch={pitch}
+        altitude={altitude}
+        maxZoom={maxZoom}
+        maxPitch={maxPitch}
+        minPitch={minPitch}
+        transitionDuration={transitionDuration}
+        transitionInterpolator={transitionInterpolator}
+        transitionInterruption={transitionInterruption}
+        width="100vw"
+        height="100vh"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/amine77/ckgkbj8w51q0x19jxhs9q0n53"
+        onViewportChange={(vport: ViewportProps) => setViewport(vport)}
+        onDblClick={(e) => addMarker(e)}
+        minZoom={12}
+        mapOptions={{
+          maxBounds: [
+            [2.65, 36.45],
+            [3.8, 36.95],
+          ],
+        }}
+      >
+        <Markers
+          isSideOpen={isSideOpen}
+          animals={animals}
+          displayAnimal={displayAnimal}
+        />
+      </ReactMapGl>
+    </div>
+  );
 }
