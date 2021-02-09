206173a13458e8baa459db88dc7d7248d5f52174
AM-77 <amine.griche77@protonmail.ch>
2021-02-09 20:55:18 +0100

    feat: update the admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { User as IUser } from 'firebase';
+import AdminFiltersBar from '../components/AdminFiltersBar';
 import ListAnimals from '../components/ListAnimals';
 import Navbar from '../components/Navbar';
 
+import { filter } from '../utils';
+
 import IAnimal from '../interfaces/IAnimal';
+import AnimalCardDetails from '../components/AnimalCardDetails';
 
 interface IProps {
   animals: IAnimal[];
@@ -13,7 +17,6 @@ interface IProps {
   setToast: (isSuccess: boolean, message: string, isHidden: boolean) => void;
   loadAnimals: (callback?: () => void) => void;
 }
-
 export default function Admin(props: IProps) {
   const {
     animals,
@@ -22,18 +25,47 @@ export default function Admin(props: IProps) {
     setToast,
     loadAnimals,
   } = props;
+  const [animalsList, setAnimalsList] = useState(animals);
+  const [animal, setAnimal] = useState<IAnimal | undefined>();
+  const [showAnimal, setShowAnimal] = useState(false);
+
+  useEffect(() => {
+    if (animal) {
+      setShowAnimal(true);
+    }
+  }, [animal]);
+
+  const filterAnimals = (sort: { date: string; type: string }) => {
+    setAnimalsList(filter(sort, animals));
+  };
+
+  const close = () => {
+    setAnimal(undefined);
+    setShowAnimal(false);
+  };
+
   return (
     <div className="admin-container">
+      {showAnimal && (
+        <AnimalCardDetails
+          animal={animal}
+          close={close}
+          setToast={setToast}
+          loadAnimals={loadAnimals}
+        />
+      )}
       <Navbar
         isLoadingAnimals={isLoadingAnimals}
         loggedUser={loggedUser}
         isAdmin
       />
       <div className="dashboard">
+        <AdminFiltersBar filterAnimals={filterAnimals} />
         <ListAnimals
           setToast={setToast}
-          animals={animals}
+          animals={animalsList}
           loadAnimals={loadAnimals}
+          setAnimal={setAnimal}
         />
       </div>
     </div>
